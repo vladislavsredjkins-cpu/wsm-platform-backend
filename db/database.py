4f4b5928bbb326ea5047e75dcc189cd85db9590f
vladislavsredjkins-cpu <vladislavs.redjkins@gmail.com>
2026-03-05 19:36:58 +0400

    Create database.py

---
+++ db/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
