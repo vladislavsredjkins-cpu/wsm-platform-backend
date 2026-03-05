eef86aa93022f43354f124ba1464813d7bbd674f
vladislavsredjkins-cpu <vladislavs.redjkins@gmail.com>
2026-03-05 23:32:04 +0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 from .athlete import Athlete
 from .competition import Competition
 from .result import Result
+
 from .competition_division import CompetitionDivision
 from .competition_discipline import CompetitionDiscipline
 from .participant import Participant
