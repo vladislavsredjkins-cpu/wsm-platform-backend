79f1f29bdb24c0df212c5e2792b905b1bc1bd5c9
vladislavsredjkins-cpu <vladislavs.redjkins@gmail.com>
2026-03-08 00:42:33 +0400

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -5,20 +5,6 @@ from sqlalchemy import create_engine
 from alembic import context
 
 from db.base import Base
-
-from models.athlete import Athlete
-from models.competition import Competition
-from models.competition_division import CompetitionDivision
-from models.competition_discipline import CompetitionDiscipline
-from models.participant import Participant
-from models.discipline_result import DisciplineResult
-from models.discipline_standing import DisciplineStanding
-from models.overall_standing import OverallStanding
-from models.protest import Protest
-from models.ranking_point import RankingPoint
-from models.ranking_snapshot import RankingSnapshot
-from models.result import Result
-from models.season import Season
 from models.weight_category import WeightCategory
 from models.team_rule import TeamRule
 
@@ -32,11 +18,7 @@ if not db_url:
     raise RuntimeError("DATABASE_URL is not set")
 
 sync_db_url = db_url.replace("postgresql+asyncpg://", "postgresql+psycopg2://")
-
-print("DB_URL =", db_url)
-print("SYNC_DB_URL =", sync_db_url)
-
-config.set_main_option("sqlalchemy.url", sync_db_url))
+config.set_main_option("sqlalchemy.url", sync_db_url)
 
 target_metadata = Base.metadata
 
