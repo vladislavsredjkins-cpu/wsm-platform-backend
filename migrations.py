1eec52818b18d41227a75f0445dbc7486333835a
vladislavsredjkins-cpu <vladislavs.redjkins@gmail.com>
2026-03-09 01:23:20 +0400

    Create migrations.py

---
+++ migrations.py
@@ -0,0 +1,7 @@
+# migrations.py
+from alembic.config import Config
+from alembic import command
+
+def run_migrations():
+    alembic_cfg = Config("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
