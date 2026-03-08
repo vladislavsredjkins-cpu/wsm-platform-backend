da7299723b7d6d542b0c0250c4407ac3830ea4f6
vladislavsredjkins-cpu <vladislavs.redjkins@gmail.com>
2026-03-08 22:43:49 +0400

    Update database.py

--- db/database.py
+++ db/database.py
@@ -1,21 +1,24 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv()  # читает .env из корня
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set")
 
 engine = create_async_engine(
     DATABASE_URL,
-    echo=False,
+    pool_pre_ping=True
 )
 
-SessionLocal = sessionmaker(
-    engine,
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
     class_=AsyncSession,
-    expire_on_commit=False,
+    expire_on_commit=False
 )
