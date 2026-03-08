f0e95ef3df71693650361e15a04878da5f1f9cd9
vladislavsredjkins-cpu <vladislavs.redjkins@gmail.com>
2026-03-08 19:55:16 +0400

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -1,30 +1,57 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from logging.config import fileConfig
-
 from sqlalchemy import create_engine
 from alembic import context
 
 from db.model_registry import target_metadata
+
+
+# --------------------------------
+# LOAD ENV VARIABLES
+# --------------------------------
+
+load_dotenv()
+
+# --------------------------------
+# ALEMBIC CONFIG
+# --------------------------------
 
 config = context.config
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+# --------------------------------
+# DATABASE URL
+# --------------------------------
+
 db_url = os.getenv("DATABASE_URL")
+
 if not db_url:
     raise RuntimeError("DATABASE_URL is not set")
 
-sync_db_url = db_url.replace("postgresql+asyncpg://", "postgresql+psycopg2://")
+# Alembic работает через sync драйвер
+sync_db_url = db_url.replace(
+    "postgresql+asyncpg://",
+    "postgresql+psycopg2://"
+)
+
 config.set_main_option("sqlalchemy.url", sync_db_url)
 
 
+# --------------------------------
+# OFFLINE MIGRATIONS
+# --------------------------------
+
 def run_migrations_offline():
     url = config.get_main_option("sqlalchemy.url")
+
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -37,8 +64,14 @@ def run_migrations_offline():
         context.run_migrations()
 
 
+# --------------------------------
+# ONLINE MIGRATIONS
+# --------------------------------
+
 def run_migrations_online():
-    connectable = create_engine(config.get_main_option("sqlalchemy.url"))
+    connectable = create_engine(
+        config.get_main_option("sqlalchemy.url")
+    )
 
     with connectable.connect() as connection:
         context.configure(
@@ -51,6 +84,10 @@ def run_migrations_online():
         with context.begin_transaction():
             context.run_migrations()
 
+
+# --------------------------------
+# ENTRYPOINT
+# --------------------------------
 
 if context.is_offline_mode():
     run_migrations_offline()
