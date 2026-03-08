e25d3d6e12e8148165246d20bd38a2268b4ef7c6
vladislavsredjkins-cpu <vladislavs.redjkins@gmail.com>
2026-03-08 11:12:21 +0400

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,9 @@
-from logging.config import fileConfig
 import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from logging.config import fileConfig
 
 from sqlalchemy import create_engine
 from alembic import context
