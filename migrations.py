4fa9f93539221da91b7787e7fc2f069c1fdacb49
vladislavsredjkins-cpu <vladislavs.redjkins@gmail.com>
2026-03-09 01:40:39 +0400

    Delete migrations.py

--- migrations.py
+++
@@ -1,7 +0,0 @@
-# migrations.py
-from alembic.config import Config
-from alembic import command
-
-def run_migrations():
-    alembic_cfg = Config("alembic.ini")
-    command.upgrade(alembic_cfg, "head")
